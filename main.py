23f06a25a33d23d679756d53ffb4aefe986561d3
opsxin <haoxin0880@gmail.com>
2019-12-10 11:56:07 +0800

    合并函数

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import re
 import logging
+import requests
 
 from bs4 import BeautifulSoup
 from datetime import datetime
@@ -10,6 +11,13 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 TOKEN = ""
 
+choice_map = {
+    1: "今日",
+    2: "三天",
+    3: "本周"
+}
+
+
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.DEBUG
@@ -17,42 +25,40 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # 因为格式都相似，所以只需要一个
-re_news = r'href="(.*?)".*>(.*?)</a>'
+re_news = r'href="(.*?)".*?>(.*?)</a>'
 news_match = re.compile(re_news)
 
 
-# def get_content():
-#     with open("thepaper.html", "r", encoding="UTF-8") as f:
-#         soup = BeautifulSoup(f, "lxml")
-#         # 热新闻
-#         newstop = str(soup.select(".list_hot"))
-#         hotnews = re.findall(news_match, newstop)
-#         # 热话题
-#         topictop = str(soup.select(".topic_hot"))
-#         hottopic = re.findall(news_match, topictop)
-#         # 热评论和热问答
-#         news_title = str(soup.select(".taq_name"))
-#         original_news = re.findall(news_match, news_title)
-#         contenttop = str(soup.select(".ansright_cont"))
-#         content = re.findall(news_match, contenttop)
-#     return hotnews, hottopic, original_news, content
+def get_paper():
+    URL = "https://www.thepaper.cn/"
+    r = requests.get(URL)
+    with open("thepaper.html", "w") as f:
+        f.writelines(r.text)
+    # return r.text
 
 
-def get_day(update, context):
+def get_content(selecter, choice, count):
     text = []
     with open("thepaper.html", "r", encoding="UTF-8") as f:
         soup = BeautifulSoup(f, "lxml")
-        # 热新闻
-        newstop = str(soup.select(".list_hot"))
-        hotnews = re.findall(news_match, newstop)
+        select_content = str(soup.select(selecter))
+        content = re.findall(news_match, select_content)
 
     text.append(
-        "今天日期是：*%s*，以下是今日热点新闻："
-        % datetime.strftime(datetime.now(), "%Y-%m-%d"))
-    for i in range(10):
-        text.append("{}：{} [thepaper.cn](www.thepaper.cn/{})".format(
-            i % 10+1, hotnews[i][1], hotnews[i][0]))
-    text = "\n".join(text)
+        "今天日期是：*{}*，以下是{}热点：".format(
+            datetime.strftime(datetime.now(), "%Y-%m-%d"),
+            choice_map[choice]))
+    for i in range((choice-1)*count, choice*count):
+        text.append(
+            "{}：{} [thepaper.cn](https://www.thepaper.cn/{})".format(
+                i % count+1, content[i][1], content[i][0]))
+    text = "\n\n".join(text)
+
+    return text
+
+
+def get_day(update, context):
+    text = get_content(".list_hot", 1, 10)
 
     context.bot.send_message(
         chat_id=update.message.chat_id,
@@ -61,20 +67,7 @@ def get_day(update, context):
 
 
 def get_days(update, context):
-    text = []
-    with open("thepaper.html", "r", encoding="UTF-8") as f:
-        soup = BeautifulSoup(f, "lxml")
-        # 热新闻
-        newstop = str(soup.select(".list_hot"))
-        hotnews = re.findall(news_match, newstop)
-
-    text.append(
-        "今天日期是：*%s*，以下是三天热点新闻："
-        % datetime.strftime(datetime.now(), "%Y-%m-%d"))
-    for i in range(10, 20):
-        text.append("{}：{} [thepaper.cn](www.thepaper.cn/{})".format(
-            i % 10+1, hotnews[i][1], hotnews[i][0]))
-    text = "\n".join(text)
+    text = get_content(".list_hot", 2, 10)
 
     context.bot.send_message(
         chat_id=update.message.chat_id,
@@ -83,20 +76,7 @@ def get_days(update, context):
 
 
 def get_week(update, context):
-    text = []
-    with open("thepaper.html", "r", encoding="UTF-8") as f:
-        soup = BeautifulSoup(f, "lxml")
-        # 热新闻
-        newstop = str(soup.select(".list_hot"))
-        hotnews = re.findall(news_match, newstop)
-
-    text.append(
-        "今天日期是：*%s*，以下是这周热点新闻："
-        % datetime.strftime(datetime.now(), "%Y-%m-%d"))
-    for i in range(20, 30):
-        text.append("{}：{} [thepaper.cn](www.thepaper.cn/{})".format(
-            i % 10+1, hotnews[i][1], hotnews[i][0]))
-    text = "\n".join(text)
+    text = get_content(".list_hot", 3, 10)
 
     context.bot.send_message(
         chat_id=update.message.chat_id,
@@ -105,20 +85,7 @@ def get_week(update, context):
 
 
 def get_day_topic(update, context):
-    text = []
-    with open("thepaper.html", "r", encoding="UTF-8") as f:
-        soup = BeautifulSoup(f, "lxml")
-        # 热话题
-        topictop = str(soup.select(".topic_hot"))
-        hottopic = re.findall(news_match, topictop)
-
-    text.append(
-        "今天日期是：*%s*，以下是今日热点话题："
-        % datetime.strftime(datetime.now(), "%Y-%m-%d"))
-    for i in range(5):
-        text.append("{}：{} [thepaper.cn](www.thepaper.cn/{})".format(
-            i % 5+1, hottopic[i][1], hottopic[i][0]))
-    text = "\n".join(text)
+    text = get_content(".topic_hot", 1, 5)
 
     context.bot.send_message(
         chat_id=update.message.chat_id,
@@ -127,20 +94,7 @@ def get_day_topic(update, context):
 
 
 def get_days_topic(update, context):
-    text = []
-    with open("thepaper.html", "r", encoding="UTF-8") as f:
-        soup = BeautifulSoup(f, "lxml")
-        # 热话题
-        topictop = str(soup.select(".topic_hot"))
-        hottopic = re.findall(news_match, topictop)
-
-    text.append(
-        "今天日期是：*%s*，以下是三天热点话题："
-        % datetime.strftime(datetime.now(), "%Y-%m-%d"))
-    for i in range(5, 10):
-        text.append("{}：{} [thepaper.cn](www.thepaper.cn/{})".format(
-            i % 5+1, hottopic[i][1], hottopic[i][0]))
-    text = "\n".join(text)
+    text = get_content(".topic_hot", 2, 5)
 
     context.bot.send_message(
         chat_id=update.message.chat_id,
@@ -149,20 +103,7 @@ def get_days_topic(update, context):
 
 
 def get_week_topic(update, context):
-    text = []
-    with open("thepaper.html", "r", encoding="UTF-8") as f:
-        soup = BeautifulSoup(f, "lxml")
-        # 热话题
-        topictop = str(soup.select(".topic_hot"))
-        hottopic = re.findall(news_match, topictop)
-
-    text.append(
-        "今天日期是：*%s*，以下是这周热点话题："
-        % datetime.strftime(datetime.now(), "%Y-%m-%d"))
-    for i in range(10, 15):
-        text.append("{}：{} [thepaper.cn](www.thepaper.cn/{})".format(
-            i % 5+1, hottopic[i][1], hottopic[i][0]))
-    text = "\n".join(text)
+    text = get_content(".topic_hot", 3, 5)
 
     context.bot.send_message(
         chat_id=update.message.chat_id,
@@ -183,12 +124,13 @@ def get_comment(update, context):
         "今天日期是：*%s*，以下是今日热评："
         % datetime.strftime(datetime.now(), "%Y-%m-%d"))
     for i in range(0, 5):
-        text.append("{}：{}\n{}[thepaper.cn](www.thepaper.cn/{})".format(
-            i % 5 + 1,
-            original_news[i][1],
-            content[i][1],
-            original_news[i][0]))
-    text = "\n".join(text)
+        text.append(
+            "{}：{}\n热评：{} [thepaper.cn](https://www.thepaper.cn/{})".format(
+                i % 5 + 1,
+                original_news[i][1],
+                content[i][1],
+                original_news[i][0]))
+    text = "\n\n".join(text)
 
     context.bot.send_message(
         chat_id=update.message.chat_id,
@@ -200,21 +142,25 @@ def get_answer(update, context):
     text = []
     with open("thepaper.html", "r", encoding="UTF-8") as f:
         soup = BeautifulSoup(f, "lxml")
-        # 热评论
+        # 热问答
         news_title = str(soup.select(".taq_name"))
         original_news = re.findall(news_match, news_title)
-        contenttop = str(soup.select(".ansright_cont"))
-        content = re.findall(news_match, contenttop)
+        select_question = str(soup.select(".taq_ct"))
+        question = re.findall(news_match, select_question)
+        select_answer = str(soup.select(".ansright_cont"))
+        answer = re.findall(news_match, select_answer)
     text.append(
         "今天日期是：*%s*，以下是今日热点问答："
         % datetime.strftime(datetime.now(), "%Y-%m-%d"))
     for i in range(5, 10):
-        text.append("{}：{}\n{}[thepaper.cn](www.thepaper.cn/{})".format(
-            i % 5 + 1,
-            original_news[i][1],
-            content[i][1],
-            original_news[i][0]))
-    text = "\n".join(text)
+        text.append(
+            "{}：{}\n问：{}\n答：{}[thepaper.cn](https://www.thepaper.cn/{})".format(
+                i % 5 + 1,
+                original_news[i][1],
+                question[i % 5][1],
+                re.sub(r"<br/>|[<<`*_>>]", "", answer[i][1]),
+                original_news[i][0]))
+    text = "\n\n".join(text)
 
     context.bot.send_message(
         chat_id=update.message.chat_id,
@@ -228,6 +174,8 @@ def error(update, context):
 
 
 if __name__ == "__main__":
+    # get_paper()
+
     updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
 
@@ -244,3 +192,4 @@ if __name__ == "__main__":
 
     updater.start_polling()
     updater.idle()
+
