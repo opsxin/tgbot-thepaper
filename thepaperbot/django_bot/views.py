d14895b841127fe0e9ad16e945d17548c5c830c3
opsxin <haoxin0880@gmail.com>
2020-01-03 17:23:36 +0800

    从入门到放弃

--- thepaperbot/django_bot/views.py
+++ thepaperbot/django_bot/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 import json
+from django.http import HttpResponse
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
 from telegram import Bot, Update
@@ -9,11 +10,15 @@ from telegram.ext import Dispatcher
 
 @csrf_exempt
 def webhook(request):
-    bot_token = settings.DJANGO_BOT.get("BOT")["TOKEN"]
-    bot = Bot(bot_token)
+    if request.method == "POST":
+        bot_token = settings.DJANGO_BOT.get("BOT")["TOKEN"]
+        bot = Bot(bot_token)
 
-    data = json.loads(request.body.decode("utf-8"))
+        data = json.loads(request.body.decode("utf-8"))
 
-    dispatcher = Dispatcher(bot)
-    update = Update.de_json(data, bot)
-    dispatcher.process_update(update)
+        dispatcher = Dispatcher(bot, None, workers=0)
+        update = Update.de_json(data, bot)
+        dispatcher.process_update(update)
+        return HttpResponse("完成")
+    else:
+        return HttpResponse("错误")
