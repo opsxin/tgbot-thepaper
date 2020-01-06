652b2203d09058ada7d38d548112629b9ad1d28a
opsxin <haoxin0880@gmail.com>
2020-01-06 15:00:45 +0800

    添加 admin 管理 MessageLog

--- thepaper_bot/bot/admin.py
+++ thepaper_bot/bot/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import MessageLog
 
 # Register your models here.
+admin.site.register(MessageLog)
--- thepaper_bot/bot/urls.py
+++ thepaper_bot/bot/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
 from . import views
 from django.conf import settings
+from django.urls import path
+from django.contrib import admin
 
 
 webhook_prefix = settings.DJANGO_BOT.get("WEBHOOK_PREFIX")
@@ -11,4 +12,5 @@ if not webhook_prefix.endswith("/"):
 
 urlpatterns = [
     path('{}'.format(webhook_prefix), views.webhook),
+    path('{}admin/'.format(webhook_prefix), admin.site.urls),
 ]
