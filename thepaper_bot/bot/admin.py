4f3dd8b0eb17a0da5b55fdfbb1c9e504acfd2e56
opsxin <haoxin0880@gmail.com>
2020-01-07 13:24:48 +0800

    Admin 页面自定义

--- thepaper_bot/bot/admin.py
+++ thepaper_bot/bot/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import MessageLog
 
 # Register your models here.
-admin.site.register(MessageLog)
+class MessageLogAdmin(admin.ModelAdmin):
+    date_hierarchy = "query_time"
+    list_filter = ['query_time']
+    empty_value_display = ""
+    list_display = ('name', 'username', 'text', 'query_time')
+
+admin.site.register(MessageLog, MessageLogAdmin)
